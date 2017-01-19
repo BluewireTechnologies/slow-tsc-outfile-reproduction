2ec3acad9a97393b597539b29ec2fbc03cb6024b
Andy Davies <andy.davies@bluewire-technologies.com>
2017-01-19 15:49:00 +0000

    Emit single text file to output directory

--- Bluewire.TypeScriptSourceGenerator/Bluewire.TypeScriptSourceGenerator/Program.cs
+++ Bluewire.TypeScriptSourceGenerator/Bluewire.TypeScriptSourceGenerator/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.IO;
 
 namespace Bluewire.TypeScriptSourceGenerator
 {
     class Program
     {
+        const string outputDirectory = @"C:\TypeScriptSourceGenerator\Output";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            var filename = "HelloWorld.txt";
+            var content = "Hello, world!" + Environment.NewLine;
+            var path = Path.Combine(outputDirectory, filename);
+
+            Directory.CreateDirectory(outputDirectory);
+            File.WriteAllText(path, content);
         }
     }
 }
