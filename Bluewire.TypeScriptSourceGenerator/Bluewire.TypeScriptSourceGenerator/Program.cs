1427353062d159df5bf1d8aa83413eee107d4467
Andy Davies <andy.davies@bluewire-technologies.com>
2017-01-20 10:45:35 +0000

    Emit TypeScript source utilising subdirectories

--- Bluewire.TypeScriptSourceGenerator/Bluewire.TypeScriptSourceGenerator/Program.cs
+++ Bluewire.TypeScriptSourceGenerator/Bluewire.TypeScriptSourceGenerator/Program.cs
@@ -5,23 +5,27 @@ namespace Bluewire.TypeScriptSourceGenerator
 {
     class Program
     {
-        const string outputDirectory = @"C:\TypeScriptSourceGenerator\Output";
+        const string rootOutputDirectory = @"C:\TypeScriptSourceGenerator\Output";
+        const string subdirectory = @".\Subdirectory";
 
         static void Main(string[] args)
         {
-            Directory.CreateDirectory(outputDirectory);
+            Directory.CreateDirectory(rootOutputDirectory);
+            Directory.CreateDirectory(Path.Combine(rootOutputDirectory, subdirectory));
 
             var mainSource =
-                "import { helloWorld } from './ExportedConstant';" + Environment.NewLine +
+                "import { helloWorld } from './Subdirectory/Module';" + Environment.NewLine +
                 "console.log(helloWorld);" + Environment.NewLine;
 
             EmitFile("Main.ts", mainSource);
-            EmitFile("ExportedConstant.ts", "export const helloWorld = 'Hello, world!';" + Environment.NewLine);
+            EmitFile("Module.ts", "export const helloWorld = 'Hello, world!';" + Environment.NewLine, subdirectory);
         }
 
-        static void EmitFile(string filename, string content)
+        static void EmitFile(string filename, string content, string subdirectory = @".\")
         {
-            var path = Path.Combine(outputDirectory, filename);
+            var directory = Path.Combine(rootOutputDirectory, subdirectory);
+            var path = Path.Combine(directory, filename);
+
             File.WriteAllText(path, content);
         }
     }
