aa473809836e088c40ab4ec88bd4a9d90e0fd1ce
Andy Davies <andy.davies@bluewire-technologies.com>
2017-01-20 11:27:01 +0000

    Emit and invoke lambdas from 30 modules

--- Bluewire.TypeScriptSourceGenerator/Bluewire.TypeScriptSourceGenerator/Program.cs
+++ Bluewire.TypeScriptSourceGenerator/Bluewire.TypeScriptSourceGenerator/Program.cs
@@ -7,18 +7,44 @@ namespace Bluewire.TypeScriptSourceGenerator
     {
         const string rootOutputDirectory = @"C:\TypeScriptSourceGenerator\Output";
         const string subdirectory = @".\Subdirectory";
+        const int modulesToGenerate = 30;
 
         static void Main(string[] args)
         {
             Directory.CreateDirectory(rootOutputDirectory);
             Directory.CreateDirectory(Path.Combine(rootOutputDirectory, subdirectory));
 
-            var mainSource =
-                "import { helloWorld } from './Subdirectory/Module';" + Environment.NewLine +
-                "console.log(helloWorld);" + Environment.NewLine;
+            CreateMainSourceFile();
+            CreateModuleSourceFiles();
+        }
 
-            EmitFile("Main.ts", mainSource);
-            EmitFile("Module.ts", "export const helloWorld = 'Hello, world!';" + Environment.NewLine, subdirectory);
+        static void CreateMainSourceFile()
+        {
+            var importLines = new string[modulesToGenerate];
+            var invokationLines = new string[modulesToGenerate];
+
+            for (var i = 0; i < modulesToGenerate; i++)
+            {
+                importLines[i] = $"import {{ lambda{i + 1} }} from './Subdirectory/Module{i + 1}';";
+                invokationLines[i] = $"lambda{i + 1}();";
+            }
+
+            var importBlock = string.Join(Environment.NewLine, importLines) + Environment.NewLine;
+            var invokationBlock = string.Join(Environment.NewLine, invokationLines) + Environment.NewLine;
+            var content = importBlock + Environment.NewLine + invokationBlock;
+
+            EmitFile("Main.ts", content);
+        }
+
+        static void CreateModuleSourceFiles()
+        {
+            for (var i = 0; i < modulesToGenerate; i++)
+            {
+                var filename = $"Module{i + 1}.ts";
+                var content = $"export const lambda{i + 1} = () => console.log('lambda{i + 1} invoked');" + Environment.NewLine;
+
+                EmitFile(filename, content, subdirectory);
+            }
         }
 
         static void EmitFile(string filename, string content, string subdirectory = @".\")
